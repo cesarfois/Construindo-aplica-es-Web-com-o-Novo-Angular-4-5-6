95c226de8d267dc051f22b67e798477777b9d963
cesarfois <fois2010@gmail.com>
2019-12-18 20:11:16 -0300

    first commit

---
+++ typescript/app.ts
@@ -0,0 +1,7 @@
+let message = "Help me, Obi-Wan Kenobi. You are my only hope!"
+console.log(message)
+
+let episode: number = 4
+console.log("This is episode " + 4)
+episode = episode + 1
+console.log("Next episode is " + episode )
