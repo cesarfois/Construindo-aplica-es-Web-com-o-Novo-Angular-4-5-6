3e24b3c2a081264744628610f79f7872a751a0df
cesarfois <fois2010@gmail.com>
2020-01-29 09:37:51 -0300

    Vue aulas 370 - 376

--- typescript/app.js
+++
@@ -1,6 +0,0 @@
-var message = "Help me, Obi-Wan Kenobi. You are my only hope!";
-console.log(message);
-var episode = 4;
-console.log("This is episode " + 4);
-episode = episode + 1;
-console.log("Next episode is " + episode);
--- typescript/app.ts
+++
@@ -1,7 +0,0 @@
-let message = "Help me, Obi-Wan Kenobi. You are my only hope!"
-console.log(message)
-
-let episode: number = 4
-console.log("This is episode " + 4)
-episode = episode + 1
-console.log("Next episode is " + episode )
